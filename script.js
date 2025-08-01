f116b80b1dc213e230013718d2e9fe54a6f3b350
jinhakang-usa <jinhakang.usa@gmail.com>
2025-08-01 18:21:26 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".hamburger");
+    const menu = document.querySelector(".menu");
+
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        menu.classList.toggle("active");
+    });
+
+    document.querySelectorAll(".nav-link").forEach(n =>
+        n.addEventListener("click", () => {
+            hamburger.classList.remove("active");
+            menu.classList.remove("active");
+        }));
+});
